c29d106d3549c7dea4a22f95c9f61ad22629c580
RyoTakanashi <K019G1339@gmail.com>
2019-09-12 15:15:35 +0900

    背景のスクロールの実装

---
+++ GameJam9/GameJam9/Actor/BackGround.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameJam9.Def;
+using GameJam9.Device;
+using GameJam9.Manager;
+using GameJam9.Util;
+using Microsoft.Xna.Framework;
+
+namespace GameJam9.Actor
+{
+    abstract class BackGround : GameObject
+    {
+        protected float speed;
+
+        public BackGround(string name, Vector2 position, float speed) 
+            : base(name, position, new Point(Screen.Width, Screen.Height))
+        {
+            this.speed = speed;
+        }
+        
+        protected override void Draw(Drawer drawer)
+        {
+            Renderer.Instance.DrawTexture(Name, Position + new Vector2(Size.X, 0), drawer);
+            base.Draw(drawer);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Position = new Vector2((GameDevice.Instance().DisplayModify.X * speed ) % Size.X, 0);
+        }
+        public override void Hit(GameObject gameObject)
+        {
+        }
+    }
+}
